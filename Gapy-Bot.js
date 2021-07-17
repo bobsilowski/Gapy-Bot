b9f1ce9bc8f7e56fd157bcc946a9d5985d4dfa5d
bobsilowski <80289558+bobsilowski@users.noreply.github.com>
2021-07-18 00:49:58 +0100

    Updated code for Gapy-Bot

--- Gapy-Bot.js
+++ Gapy-Bot.js
@@ -11,8 +11,66 @@ client.once('ready', () => {
     client.channels.cache.get("827179141039915058").send(awakeEmbed);
 });
 
-client.on("message", (message) => {
+client.on("messageReactionAdd", async (reaction, user, message) => { //Add an event listener
+    if (reaction.message.partial) await reaction.message.fetch();
+  
+    if (user.id === client.user.id) return; //If the reaction was from the bot, return
+    if (!reaction.message.guild) return; //If the reaction was not in a guild
+  
+    if (reaction.emoji.name === "🎫" && client.channel.id == "809040643593994251" || reaction.emoji.name === ":ticket:" && reaction.channel.id == "809040643593994251") {
+        client.guild.channels.create(`ticket ${message.author.username}`, {
+        permissionOverwrites: [
+        {
+            id: message.guild.roles.everyone,
+            deny: ['VIEW_CHANNEL'],
+        },
+        {
+            id: message.author.id,
+            allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
+        },
+        {
+            id: "745973047131111535",
+            allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],   
+        },
+        {
+            id: "745957715678986382",
+            allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],   
+        },
+        {
+            id: "816622827494178836",
+            allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],   
+        },
+        {
+            id: "816622821685985320",
+            allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],   
+        },
+        {
+            id: "811660541118382111",
+            allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],   
+        },
+        {
+            id: "700035991234412576",
+            allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],   
+        },
+        ],
+    type: "text",
+    parent: '827269571253108757',
+ });
+}
+})
+
+client.on("message", async message => {
     if (message.author.bot) return
+    
+    if (message.channel.id == "838393649691099176" && message.content.includes(".link")) {
+    } else if (message.channel.id == "838393649691099176") {
+        message.delete()
+        message.channel.send(":x: Please only use this channel to `.link` your account.").then(msg => {
+            setTimeout(() => {
+             msg.delete()
+            }, 5000);
+        })
+    }
 
     const args = message.content.slice(1).trim().split(' ');
     const command = args.shift().toLowerCase(); //not bad here btw
@@ -38,6 +96,163 @@ client.on("message", (message) => {
             message.delete();
             message.channel.send(":x: You do not have the required permissions to run this command.");
         }
+    }
+    
+
+    if (command == "ticket-open") {
+        if (message.channel == "866059633953472512") {
+            message.guild.channels.create(`ticket ${message.author.username}`, {
+                 permissionOverwrites: [
+                   {
+                     id: message.guild.roles.everyone,
+                     deny: ['VIEW_CHANNEL'],
+                   },
+                   {
+                     id: message.author.id,
+                     allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
+                   },
+                   {
+                    id: "745973047131111535",
+                    allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],   
+                   },
+                   {
+                    id: "745957715678986382",
+                    allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],   
+                   },
+                   {
+                    id: "816622827494178836",
+                    allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],   
+                   },
+                   {
+                    id: "816622821685985320",
+                    allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],   
+                   },
+                   {
+                    id: "811660541118382111",
+                    allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],   
+                   },
+                   {
+                    id: "700035991234412576",
+                    allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],   
+                   },
+                    ],
+                type: "text",
+                parent: '827269571253108757',
+              }),
+            message.delete(),
+            message.channel.send("Creating a ticket for you now").then(msg => {
+                setTimeout(() => {
+                 msg.delete()
+                }, 5000);
+            });
+        }
+    }
+
+    if (command == "bugreport") {
+        if (message.channel == "866059633953472512") {
+            message.guild.channels.create(`bugreport ${message.author.username}`, {
+                 permissionOverwrites: [
+                   {
+                     id: message.guild.roles.everyone, 
+                     deny: ['VIEW_CHANNEL'], 
+                   },
+                   {
+                     id: message.author.id,
+                     allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
+                   },
+                   {
+                    id: "815952114521604096",
+                    allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
+                  },
+                    ],
+                type: "text",
+                parent: '827269571253108757',
+              }),
+            message.delete(),
+            message.channel.send("Creating a ticket for you now").then(msg => {
+                setTimeout(() => {
+                 msg.delete()
+                }, 5000);
+            })
+        }
+    }
+
+    const setupTickets = new Discord.MessageEmbed()
+            .setAuthor("Gapy")
+            .setTitle("Tickets")
+            .setDescription("🎫 - General Support \n⛑️ - Staff Application \n➕ - Suggestion \n🐛 - Bug Report")
+
+    if (message.content == "!setuptickets" && message.channel.id == "809040643593994251" && message.member.roles.cache.some(r => r.name == "Admin") || message.content == "!setuptickets" && message.channel.id == "809040643593994251" && message.member.roles.cache.some(r => r.name == "Manager") || message.content == "!setuptickets" && message.channel.id == "809040643593994251" && message.member.roles.cache.some(r => r.name == "Owner")) {
+        client.channels.cache.get("809040643593994251").send(setupTickets)
+    };
+
+    if (command == "staffapply") {
+        if (message.channel == "866059633953472512") {
+            message.guild.channels.create(`ticket ${message.author.username}`, {
+                 permissionOverwrites: [
+                   {
+                     id: message.guild.roles.everyone, 
+                     deny: ['VIEW_CHANNEL'], 
+                   },
+                   {
+                     id: message.author.id,
+                     allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
+                   }
+                    ],
+                type: "text",
+                parent: '827269571253108757',
+              }),
+            message.delete(),
+            message.channel.send("Creating a ticket for you now").then(msg => {
+                setTimeout(() => {
+                 msg.delete()
+                }, 5000);
+            })
+        }
+    }
+
+    if (command == "mediaapply") {
+        if (message.channel == "866059633953472512") {
+          message.guild.channels.create(`application ${message.author.username}`, {
+            permissionOverwrites: [
+              {
+                id: message.guild.roles.everyone,
+                deny: ['VIEW_CHANNEL'],
+              },
+              {
+                id: message.author.id,
+                allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
+              },
+              {
+                id: "821855800409718854",
+                allow: ['VIEW_CHANNEL', 'SEND_MESSAGES'],
+              },
+              {
+                id: "821855802695614494",
+                allow: ['VIEW_CHANNEL', 'SEND_MESSAGES'],
+              },
+            ],
+            type: "text",
+            parent: '866075437746749470',
+          });
+      
+          message.delete();
+      
+          message.channel.send("Creating a ticket for you now").then((msg) => {
+            setTimeout(() => {
+              msg.delete();
+            }, 5000);
+          });
+        }
+      }
+
+    if (command == "close" && message.member.roles.cache.some(r => r.name == "Admin") || command == "close" && message.member.roles.cache.some(r => r.name == "Manager") || command == "close" && message.member.roles.cache.some(r => r.name == "Owner")) {
+        message.channel.send(`Ticket closing in 5 seconds by ${message.author.username}`)
+        setTimeout(() => {
+            message.channel.delete()
+           }, 5000);
+        
+    } else {
     }
 
     if (command == "changelog") {
@@ -130,7 +345,7 @@ client.on("message", (message) => {
             .setAuthor(`From ${message.author.tag}`, message.author.displayAvatarURL({
                 dynamic: true
             }))
-            .setDescription(`${message.author} has submitted a suggestion!\n\n\`${suggestion}\``)
+            .setDescription(`${message.author} has submitted the suggestion:\n\n\`${suggestion}\``)
             .setFooter("Gapy")
             .setTimestamp()
             .setColor("#3498DB")
